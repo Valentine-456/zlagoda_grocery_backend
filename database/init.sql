5d763cc2f73f61f61cac6294ad17ec8a8f616f66
Valentyn Bondarenko <murica.it@gmail.com>
2023-06-03 18:47:33 +0200

    Add constraints to the DB's fields

--- database/init.sql
+++ database/init.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS Customer_Card (
   city VARCHAR(50) NULL,
   street VARCHAR(50) NULL,
   zip_code VARCHAR(9) NULL,
-  percent INT NULL
+  percent INT NULL CHECK (percent > 0)
 );
 
 CREATE TABLE IF NOT EXISTS Employee (
@@ -30,9 +30,10 @@ CREATE TABLE IF NOT EXISTS Employee (
   empl_name VARCHAR(50) NOT NULL,
   empl_surname VARCHAR(50) NOT NULL,
   empl_patronimic VARCHAR(50) NULL,
-  empl_role VARCHAR(10) NOT NULL,
-  salary NUMERIC(13, 4) NOT NULL,
-  date_of_birth DATE NOT NULL,
+  empl_role VARCHAR(10) NOT NULL CHECK (empl_role in ('MANAGER', 'CASHIER')),
+  salary NUMERIC(13, 4) NOT NULL CHECK (salary > 0),
+  date_of_birth DATE NOT NULL 
+  CHECK (AGE(current_date, date_of_birth) > INTERVAL '18 years'),
   date_of_start DATE NOT NULL,
   phone_number VARCHAR(13) NOT NULL,
   city VARCHAR(50) NOT NULL,
@@ -45,7 +46,7 @@ CREATE TABLE IF NOT EXISTS public.Check (
   id_employee VARCHAR(10) NOT NULL,
   card_number VARCHAR(13) NULL,
   print_date DATE NOT NUll,
-  sum_total NUMERIC(13, 4) NOT NULL,
+  sum_total NUMERIC(13, 4) NOT NULL CHECK (sum_total > 0),
   vat NUMERIC(13, 4) NOT NULL GENERATED ALWAYS AS (sum_total * 0.2) STORED,
   FOREIGN KEY(id_employee)
         REFERENCES Employee (id_employee)
@@ -59,8 +60,8 @@ CREATE TABLE IF NOT EXISTS Store_Product (
   UPC VARCHAR(12) PRIMARY KEY,
   UPC_Prom VARCHAR(12) NULL,
   id_product INT NOT NULL,
-  selling_price NUMERIC(13, 4) NOT NULL,
-  products_nubmer INT NOT NULL,
+  selling_price NUMERIC(13, 4) NOT NULL CHECK (selling_price > 0),
+  products_nubmer INT NOT NULL CHECK (products_nubmer > 0),
   promotional_product BOOLEAN NOT NULL,
   FOREIGN KEY(UPC_Prom)
         REFERENCES Store_Product (UPC)
@@ -73,8 +74,8 @@ CREATE TABLE IF NOT EXISTS Store_Product (
 CREATE TABLE IF NOT EXISTS Sale (
   UPC VARCHAR(12) NOT NULL,
   check_number VARCHAR(10) NOT NULL,
-  product_number INT NOT NULL,
-  selling_price NUMERIC(13, 4) NOT NULL,
+  product_number INT NOT NULL CHECK (product_number > 0),
+  selling_price NUMERIC(13, 4) NOT NULL CHECK (selling_price > 0),
   PRIMARY KEY(UPC, check_number),
   FOREIGN KEY(UPC)
         REFERENCES Store_Product (UPC)
